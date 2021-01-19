89522817374d5b66f1a8a9aad2e05b14438b8c41
Brenn_ZA <bswanepoel4025@gmail.com>
2021-01-19 19:05:01 +0200

    move axios.get request to generateResults function, add try and catch statement

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const form = document.querySelector("#form");
 
-form.addEventListener("submit", async (event) => {
+form.addEventListener("submit", (event) => {
     event.preventDefault();
     const searchQuery = form.elements.searchInput.value;
-    const res = await axios.get(`http://musicbrainz.org/ws/2/artist/?query=artist:${searchQuery}`);
-    const artists = res.data.artists;
-    console.log(artists);
-})
+    generateResults(searchQuery);
+});
+
+const generateResults = async (searchQuery) => {
+    try {
+        const res = await axios.get(`http://musicbrainz.org/ws/2/artist/?query=artist:${searchQuery}`);
+        const artists = res.data.artists;
+        console.log(artists);
+    } catch (err) {
+        console.log(err);
+    }
+}
