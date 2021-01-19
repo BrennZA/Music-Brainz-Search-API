7725d5cc1c0a520edbdb15c2eb9869f3288260e6
Brenn_ZA <bswanepoel4025@gmail.com>
2021-01-19 19:32:26 +0200

    create displayResults function, display artist name to results container

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const form = document.querySelector("#form");
+const resultsContainer = document.querySelector("#resultsContainer");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -9,9 +10,22 @@ form.addEventListener("submit", (event) => {
 const generateResults = async (searchQuery) => {
     try {
         const res = await axios.get(`http://musicbrainz.org/ws/2/artist/?query=artist:${searchQuery}`);
-        const artists = res.data.artists;
-        console.log(artists);
+        const results = res.data.artists;
+        displayResults(results);
     } catch (err) {
         console.log(err);
+    }
+}
+
+const displayResults = (results) => {
+    for(let result of results) {
+        const artistContainer = document.createElement("div");
+        const artistHeader = document.createElement("h3");
+
+        artistHeader.innerText = result.name;
+
+        artistContainer.appendChild(artistHeader);
+
+        resultsContainer.appendChild(artistContainer);
     }
 }
