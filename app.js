5611ba929657e4f349c02c52974ab040ac588436
Brenn_ZA <bswanepoel4025@gmail.com>
2021-01-19 06:38:54 +0200

    save res.data to a variable

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ form.addEventListener("submit", async (event) => {
     event.preventDefault();
     const searchQuery = form.elements.searchInput.value;
     const res = await axios.get(`http://musicbrainz.org/ws/2/artist/?query=artist:${searchQuery}`);
-    console.log(res.data);
+    const artists = res.data.artists;
+    console.log(artists);
 })
